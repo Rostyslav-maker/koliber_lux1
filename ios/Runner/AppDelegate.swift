047ad844ea91562434eb28725ebf0ff39ee7616d
Rostyslav-maker <154871146+Rostyslav-maker@users.noreply.github.com>
2026-03-19 21:31:53 +0100

    Add Firebase initialization to AppDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import FirebaseCore // <--- DODAJ TO
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    FirebaseApp.configure() // <--- DODAJ TO PRZED REJESTRACJĄ PLUGINÓW
+    
+    GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
